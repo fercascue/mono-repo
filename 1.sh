eafdc2f6545e0637ad068be85948f623c5bfd2e3
fercascue <fercascue@gmail.com>
2023-01-03 22:19:54 +0100

    meger

--- 1.sh
+++ 1.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
-
-readarray -t my_array < <(git diff --dirstat=files,0 HEAD~1 | jq -r '.[].source' )
-file_changes=()
-# Start the JSON
-file_changes+=( {"\"files\":[" )
-
-# loop through the images, and output the JSON
-# keep track of the index of output items
-counter=1
-for source in "${my_array[@]}"
-do
-    file_changes+=( "{\"source\":\"$source\"}" )
-    # Add a comma unless it is the last element in the array
-    if [ $counter -lt ${#my_array[@]} ]
-    then
-        file_changes+=( ",")
-    else
-        file_changes+=( "")
-    fi
-    (( counter = counter + 1 ))
+sample='[{"name":"foo"},{"name":"bar"}]'
+for row in $(echo "${sample}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+   echo $(_jq '.name')
 done
-
-# Close the JSON
-file_changes+=("]}")
-echo "${file_changes[*]}"
