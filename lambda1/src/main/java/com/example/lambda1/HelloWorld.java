00d1e93c72b8ce54c312ffd6eef56e6aa95534ce
fercascue <fercascue@gmail.com>
2023-01-06 08:08:17 +0100

    test

--- lambda1/src/main/java/com/example/lambda1/HelloWorld.java
+++ lambda1/src/main/java/com/example/lambda1/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example.lambda1;
 
 public class HelloWorld {
 
-	/***
-	 * Documentation
-	 * @param args
-	 */
+ /***
+  * Documentation.
+  * @param args
+ */
   private static void main(final String[] args) {
      System.out.println("Welcome to our application");
   }
