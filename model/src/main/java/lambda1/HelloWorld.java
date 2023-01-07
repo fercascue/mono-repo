14e52c0ecbcd2cf72f8f22be3942d8320e9c924f
fercascue <fercascue@gmail.com>
2023-01-07 12:01:59 +0100

    test

---
+++ model/src/main/java/lambda1/HelloWorld.java
@@ -0,0 +1,12 @@
+package lambda1;
+
+/**
+ * 
+ * @author Usuario
+ *
+ */
+public class HelloWorld {
+	public static int getLucky() {
+        return 7;
+    }
+}
---
+++ model/src/main/java/lambda1/MessageBuilder.java
@@ -0,0 +1,16 @@
+package lambda1;
+
+/**
+ * 
+ * @author Usuario
+ *
+ */
+public class MessageBuilder {
+    public static String getHelloWorld(){
+        return "hello world";
+    }
+
+    public static int getNumber10(){
+        return 10;
+    }
+}
